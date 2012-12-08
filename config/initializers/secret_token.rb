f3b28d31cb2b48ced88c869a9eddca7e584902d1
Hephzibah <hephzibah.1989@gmail.com>
2012-12-08 13:26:59 +0530

    @Rails Girls making a blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirlsblog::Application.config.secret_token = 'd4450b7e8cdbb2ed5797923da90cde585725ca285837638486e52f3902b04d4c695b6afeab998ac09b96c496f5c7e5d8317b5d9f60f79e5baed19be07970c583'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Railsgirlsblog::Application.config.session_store :cookie_store, key: '_railsgirlsblog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Railsgirlsblog::Application.config.session_store :active_record_store
